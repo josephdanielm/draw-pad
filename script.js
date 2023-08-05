62c447ae77dcc451c725d9b7ce7a0ab8157bdadf
Joseph Mudd <josephmuddjr@gmail.com>
2023-08-05 13:48:43 -0500

    Add Grid-drawing

--- script.js
+++ script.js
@@ -1,9 +1,51 @@
+// Slider
 const sliderOutput = document.getElementById('slider-value');
-let sliderValue;
+const slider = document.getElementById('slider')
+let sliderValue = 16;
 
-const slider = document.getElementById('slider').oninput = function() {
+slider.oninput = function() {
     sliderOutput.innerHTML = `${this.value} x ${this.value}`;
     sliderValue = this.value;
+    document.getElementById('pad').replaceChildren(); // Clears grid of all current divs
+    drawGrid(sliderValue);
 }
 
+
+
+// Pad
+function drawGrid(quantity) {
+    for (i = 0; i < quantity; ++i) {
+        const column = document.createElement('div');
+        column.setAttribute('class', 'column');
+        for (j = 0; j < quantity; ++j) {
+            const row = document.createElement('div');
+            row.setAttribute('class', 'padDiv');
+            column.appendChild(row);
+        }
+        document.getElementById('pad').appendChild(column);
+    }
+}
+drawGrid(sliderValue);
+
+
+// Color Picker
+
+const colorPicker = document.getElementById('color-picker');
+let currentColor = colorPicker.value;
+
+colorPicker.oninput = function() {
+    currentColor = this.value;
+}
+
+
+// Draw Effect
+
+const padDivs = document.querySelectorAll('padDiv');
+let isDrawing = false;
+
+pad.onmousedown = function(){isDrawing = true
+console.log('mouse is down')};
+
+pad.onmouseup = function(){isDrawing = false
+console.log('mouse is up')};
 
